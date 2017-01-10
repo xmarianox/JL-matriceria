3792877d82924e2ab8fe354fecb7c93e66eddd05
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-10 00:10:09 -0300

    Update form styles

--- src/components/ContactBlock.js
+++ src/components/ContactBlock.js
@@ -9,6 +9,24 @@ class ContactBlock extends Component {
                     <h2>contacto</h2>
 
                     <p>Luis María Drago 5252, Munro, Buenos Aires<br/> 011 4761-7305</p>
+
+                    <form>
+                        <div className="form-control">
+                            <input type="text" name="contact-name" id="contact-name" placeholder="Nombre" />
+                        </div>
+
+                        <div className="form-control">
+                            <input type="email" name="contact-email" id="contact-email" placeholder="Email" />
+                        </div>
+
+                        <div className="form-control">
+                            <textarea name="contact-text" id="contact-text" placeholder="Consulta"></textarea>
+                        </div>
+
+                        <div className="form-control">
+                            <button className="btn btn_action">Enviar</button>
+                        </div>
+                    </form>
                 </div>
 
                 <div className="ContactBlock-item">
