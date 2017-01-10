09bb9af50f72d512b6df52fa6b772e16a6a47a4f
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-09 21:00:59 -0300

    Add Merchandising component

---
+++ src/components/Merchandising.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+// assets
+import '../styles/components/Merchandising.scss';
+import imgMerchandising from '../images/img-merchandising.jpg';
+
+class Merchandising extends Component {
+
+    render() {
+        return (
+            <article className="MerchandisingComponent">
+                <div className="merchandising-item">
+                    <img src={imgMerchandising} alt="Merchandising" />
+                </div>
+                <div className="merchandising-item">
+                    <h2>merchandising</h2>
+
+                    <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare.  tellus a odio tincidunt auctor a  velit. Nam nec tellus a odio tincidunt auctor a ornare.  tellus a odio tincid
+ velit. Nam nec tellus a odio tincidunt auctor a ornare.  tellus a odio tincidunt auctor a</p>
+
+                    <button className="btn">hacé tu pedido</button>
+                </div>
+            </article>
+        );
+    }
+
+}
+
+export default Merchandising;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../styles/containers/Home.scss';
 
 import MainImageSlider from '../components/MainImageSlider';
-
+import Merchandising from '../components/Merchandising';
 
 class App extends Component {
   render() {
@@ -10,6 +10,8 @@ class App extends Component {
       <section className="home-container">
           
         <MainImageSlider />
+
+        <Merchandising />
 
       </section>
     )
