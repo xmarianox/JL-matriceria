7b6eae0c6e2a9ff323046b56833797a8b1b5dd72
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-09 23:50:21 -0300

    Add ContactBlock component

---
+++ src/components/ContactBlock.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class ContactBlock extends Component {
+    render() {
+        return (
+            <article>
+                <div>
+                    <h2>contacto</h2>
+
+                    <p>Luis María Drago 5252, Munro, Buenos Aires 011 4761-7305</p>
+                </div>
+
+                <div>
+                    
+                </div>
+            </article>
+        );
+    }
+}
+
+export default ContactBlock;
