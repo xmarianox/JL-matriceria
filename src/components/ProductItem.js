552ca3d47d745a5f77fc634d36de4b3e0ce79779
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-13 00:12:35 -0300

    Add styles

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,12 +3,12 @@ import React, {Component} from 'react';
 class ProductItem extends Component {
     render() {
         return (
-            <li>
+            <li className="ProductItem">
                 <figure>
-                    <img src={this.props.image} alt={this.props.name} />
+                    <img src={this.props.product.image} alt={this.props.product.name} />
                     <figcaption>
-                        <span>{this.props.name}</span>
-                        <span>{this.props.price}</span>
+                        <span>{this.props.product.name}</span>
+                        <span>{this.props.product.price}</span>
                     </figcaption>
                 </figure>
             </li>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,69 @@
 import React, {Component} from 'react';
 
+import '../styles/components/ProductList.scss';
 import ProductItem from './ProductItem';
 
 class ProductList extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            list: [
+                {
+                    id: 1,
+                    name: 'Vaso',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '35.00'
+                },
+                {
+                    id: 2,
+                    name: 'Vaso 2',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '39.00'
+                },
+                {
+                    id: 3,
+                    name: 'Vaso 3',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '32.00'
+                },
+                {
+                    id: 4,
+                    name: 'Vaso 4',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '45.00'
+                },
+                {
+                    id: 5,
+                    name: 'Vaso 5',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '55.00'
+                },
+                {
+                    id: 6,
+                    name: 'Vaso 6',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '39.00'
+                },
+                {
+                    id: 7,
+                    name: 'Vaso 7',
+                    image: 'http://placehold.it/350x150/E8117F/000000',
+                    price: '34.00'
+                }
+            ]
+        };
+    }
+
     render() {
         return (
-            <ul>
-                <ProductItem name="" image="" price="" />
+            <ul className="ProductList">
+                {
+                    this.state.list.map((item) => {
+                        return <ProductItem product={item} key={item.id} />
+                    })
+                }
             </ul>
         );
     }
