9f0b71f90913a97ebff38a289df7c033284c4994
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-14 10:41:12 -0300

    Update

--- src/components/CatalogBlock.js
+++ src/components/CatalogBlock.js
@@ -23,6 +23,32 @@ class CatalogBlock extends Component {
                     id: 3,
                     label: 'TUPPERS'
                 }
+            ],
+            list: [
+                {
+                    id: 1,
+                    name: 'Vaso',
+                    image: 'http://placehold.it/195x250/E8117F/000000',
+                    price: '35.00'
+                },
+                {
+                    id: 2,
+                    name: 'Vaso 2',
+                    image: 'http://placehold.it/195x250/E8117F/000000',
+                    price: '39.00'
+                },
+                {
+                    id: 3,
+                    name: 'Vaso 3',
+                    image: 'http://placehold.it/195x250/E8117F/000000',
+                    price: '32.00'
+                },
+                {
+                    id: 4,
+                    name: 'Vaso 4',
+                    image: 'http://placehold.it/195x250/E8117F/000000',
+                    price: '45.00'
+                }
             ]
         };
     }
@@ -35,9 +61,9 @@ class CatalogBlock extends Component {
                     <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare. tellus a odio tincidunt auctor a</p>
                 </div>
                 
-                <CatalogSelector items={this.state.catalog} />
+                <CatalogSelector items={this.state.catalog}  />
 
-                <ProductList />
+                <ProductList productList={this.state.list} />
             </article>
         );
     }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,44 +5,10 @@ import ProductItem from './ProductItem';
 
 class ProductList extends Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            list: [
-                {
-                    id: 1,
-                    name: 'Vaso',
-                    image: 'http://placehold.it/195x250/E8117F/000000',
-                    price: '35.00'
-                },
-                {
-                    id: 2,
-                    name: 'Vaso 2',
-                    image: 'http://placehold.it/195x250/E8117F/000000',
-                    price: '39.00'
-                },
-                {
-                    id: 3,
-                    name: 'Vaso 3',
-                    image: 'http://placehold.it/195x250/E8117F/000000',
-                    price: '32.00'
-                },
-                {
-                    id: 4,
-                    name: 'Vaso 4',
-                    image: 'http://placehold.it/195x250/E8117F/000000',
-                    price: '45.00'
-                }
-            ]
-        };
-    }
-
     render() {
         return (
             <ul className="ProductList">
-                {
-                    this.state.list.map((item) => {
+                { this.props.productList.map((item) => {
                         return <ProductItem product={item} key={item.id} />
                     })
                 }
