b310bb4d4081879a7d6c4aab193bcdcfda1db449
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-14 16:54:05 -0300

    Add catalogo route

---
+++ src/containers/Catalogo.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+// container styles
+import '../styles/containers/Home.scss';
+// components
+import MainImageSlider from '../components/MainImageSlider';
+import CatalogBlock    from '../components/CatalogBlock';
+import ContactBlock    from '../components/ContactBlock';
+// assets
+import images from '../images/img-slider.jpg';
+
+class Catalogo extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            mainImages: [
+            {
+                original: images,
+                originalAlt: 'Promo 1',
+                description: {
+                    new_product: true,
+                    title: 'Jl Matrceria web-22',
+                    label: 'Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare.',
+                }
+            },
+            {
+                original: images,
+                originalAlt: 'Promo 2',
+                description: {
+                    new_product: true,
+                    title: 'Jl Matrceria web-23',
+                    label: 'Orbi accumsan ipsum velit.'
+                }
+            },
+            {
+                original: images,
+                originalAlt: 'Promo 3',
+                description: {
+                    new_product: true,
+                    title: 'Jl Matrceria web-24',
+                    label: 'Orbi accumsan ipsum velit.',
+                }
+            }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <section className="catalogo-container">
+                <MainImageSlider productImages={this.state.mainImages} />
+
+                <CatalogBlock />
+
+                <ContactBlock />
+            </section>
+        );
+    }
+
+}
+
+export default Catalogo;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/App';
 import Home from './containers/Home';
+import Catalogo from './containers/Catalogo';
 
 const routes = () => {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
+      <Route path="/catalogo" component={Catalogo}/>
     {/*
       <Route path="/proyectos" component={Proyectos}/>
       <Route path="/proyectos/:proyecto_slug" component={ProyectoDetalle}/>
