23808bc5e17116085892ac63cf8d246434843b5c
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-14 10:36:55 -0300

    Update selector style

--- src/components/CatalogBlock.js
+++ src/components/CatalogBlock.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+
+import CatalogSelector from './CatalogSelector';
 import ProductList from './ProductList';
 import '../styles/components/CatalogBlock.scss';
 
 class CatalogBlock extends Component {
 
-    
+    constructor() {
+        super();
+
+        this.state = {
+            catalog: [
+                {
+                    id: 1,
+                    label: 'VASOS'
+                },
+                {
+                    id: 2,
+                    label: 'SERVILLETEROS'
+                },
+                {
+                    id: 3,
+                    label: 'TUPPERS'
+                }
+            ]
+        };
+    }
+
     render() {
         return (
             <article className="CatalogBlock">
@@ -12,11 +34,14 @@ class CatalogBlock extends Component {
                     <h2>catálogo</h2>
                     <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare. tellus a odio tincidunt auctor a</p>
                 </div>
-   
+                
+                <CatalogSelector items={this.state.catalog} />
+
                 <ProductList />
             </article>
         );
     }
+
 }
 
 export default CatalogBlock;
--- src/components/CatalogSelector.js
+++ src/components/CatalogSelector.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react';
+import '../styles/components/CatalogSelector.scss';
 
 class CatalogSelector extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            activeIndex: 1
+        };
+    }
+
+    _handleToggle(index) {
+        this.setState({ activeIndex: index });
+    }
+
     render() {
+
+        let active = this.state.activeIndex;
+
         return (
-            <ul>
+            <ul className="CatalogSelector">
                 { this.props.items.map((item) => {
-                        return <li key={item.id}>{item.lable}</li>
+                        return <li key={item.id} onClick={this._handleToggle.bind(this, item.id)} className={item.id === active ? 'active' : ''}>{item.label}</li>
                     })
                 }
             </ul>
         );
     }
+
+
+
 }
 
 export default CatalogSelector;
