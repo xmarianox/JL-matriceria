38c3a4d4b25f49156e8897a4c9a5794ea590705d
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-18 00:06:22 -0300

    mock product detail view

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -7,7 +7,48 @@ class ProductDetail extends Component {
             <section className="product-detail-container">
 
                 <article>
-                    <img src="http://placehold.it/195x250/E8117F/000000" alt="image" />
+                    <div>
+                        <img src="http://placehold.it/195x250/E8117F/000000" alt="image" />
+                    </div>{/* row */}
+
+                    <div>
+                        <div>
+                            <span>NEW!</span>
+
+                            <h2>Jl Matrceria web-22</h2>
+
+                            <span className="product-price">$150</span>
+
+                            <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare.</p>
+                        </div>
+
+                        <div>
+                            <em>color</em>
+
+                            <ul>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <em>tamaño</em>
+
+                            <ul>
+                                <li>S</li>
+                                <li>M</li>
+                                <li>L</li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <em>cantidad</em>
+                        </div>
+
+                    </div>{/* row */}
+                    
                 </article>
 
             </section>
