ee5b4e153683796115562cc9608ed6a9c483cd0c
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-18 00:34:04 -0300

    mock product detail view

---
+++ src/components/ProductDetailBlock.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import '../styles/components/ProductDetailBlock.scss';
+
+
+class ProductDetailBlock extends Component {
+    render() {
+        return (
+            <article className="ProductDetailBlock">
+                <div className="row">
+                    <img className="pd-image" src="http://placehold.it/414x350/E8117F/000000" alt="image" />
+                </div>{/* row */}
+
+                <div className="row">
+                    <div className="pd-info-container">
+                        <div className="pd-item-container">
+                            <span>NEW!</span>
+
+                            <h2>Jl Matrceria web-22</h2>
+
+                            <span className="pd-price">$150</span>
+
+                            <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare.</p>
+                        </div>
+
+                        <div className="pd-item-container">
+                            <em>color</em>
+
+                            <ul>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                                <li>rojo</li>
+                            </ul>
+                        </div>
+
+                        <div className="pd-item-container">
+                            <em>tamaño</em>
+
+                            <ul>
+                                <li>S</li>
+                                <li>M</li>
+                                <li>L</li>
+                            </ul>
+                        </div>
+
+                        <div className="pd-item-container">
+                            <em>cantidad</em>
+                        </div>
+                    </div>
+
+                </div>{/* row */}
+            </article>
+        );
+    }
+}
+
+export default ProductDetailBlock;
--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+// styles
+import '../styles/containers/ProductDetail.scss';
+
+// components
+import ProductDetailBlock from '../components/ProductDetailBlock';
 
 class ProductDetail extends Component {
 
@@ -6,50 +11,7 @@ class ProductDetail extends Component {
         return (
             <section className="product-detail-container">
 
-                <article>
-                    <div>
-                        <img src="http://placehold.it/195x250/E8117F/000000" alt="image" />
-                    </div>{/* row */}
-
-                    <div>
-                        <div>
-                            <span>NEW!</span>
-
-                            <h2>Jl Matrceria web-22</h2>
-
-                            <span className="product-price">$150</span>
-
-                            <p>Orbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare.</p>
-                        </div>
-
-                        <div>
-                            <em>color</em>
-
-                            <ul>
-                                <li>rojo</li>
-                                <li>rojo</li>
-                                <li>rojo</li>
-                                <li>rojo</li>
-                            </ul>
-                        </div>
-
-                        <div>
-                            <em>tamaño</em>
-
-                            <ul>
-                                <li>S</li>
-                                <li>M</li>
-                                <li>L</li>
-                            </ul>
-                        </div>
-
-                        <div>
-                            <em>cantidad</em>
-                        </div>
-
-                    </div>{/* row */}
-                    
-                </article>
+                <ProductDetailBlock />
 
             </section>
         );
