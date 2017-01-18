f92ee3b7a98848c2b94f907e2761ba4942ea8b21
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-17 22:49:06 -0300

    Add select lib, update styles for select

--- src/components/CatalogOrderSelector.js
+++ src/components/CatalogOrderSelector.js
@@ -1,18 +1,44 @@
 import React, {Component} from 'react';
 import '../styles/components/CatalogOrderSelector.scss';
-
+//select component
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class CatalogOrderSelector extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            options : [
+                { value: 'default', label: 'Ordenar por' },
+                { value: 'mas_relevantes', label: 'Más relevantes' },
+                { value: 'menor_precio', label: 'Menor precio' },
+                { value: 'mayor_precio', label: 'Mayor precio' }
+            ],
+            active: 'default'
+        };
+    }
+
+    _handleChange(item) {
+        // console.log(`event: ${item.label}`);
+        this.setState({
+            active: item.value
+        });
+    }
 
     render() {
         return (
             <div className="CatalogOrderSelector">
-                <select>
-                    <option>Ordenar por</option>
-                    <option value="mas_relevantes">Más relevantes</option>
-                    <option value="menor_precio">Menor precio</option>
-                    <option value="mayor_precio">MAyor precio</option>
-                </select>
+                <Select 
+                    name="catalog-order"
+                    value={this.state.active}
+                    clearable={false}
+                    searchable={false}
+                    options={this.state.options}
+                    className="order-selector"
+                    onChange={this._handleChange.bind(this)}
+                />
             </div>
         );
     }
