96af7179d23e9648774d97c2737e66768a67cfcd
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-28 17:03:38 -0300

    Add desktop nav

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import '../styles/components/Nav.scss';
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav className="NavDesktop">
+                <ul>
+                    <li>
+                        <span>CATÁLOGO</span>
+                        <ul>
+                            <li><Link to="/catalogo/vasos">Vasos</Link></li>
+                            <li><Link to="/catalogo/servilleteros">Servilleteros</Link></li>
+                            <li><Link to="/catalogo/tuppers">Tuppers</Link></li>
+                        </ul>
+                    </li>
+                    <li>MERCHANDISING</li>
+                    <li>CONTACTO</li>
+                    <li>MEDIOS DE PAGO</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
+
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import '../styles/main.scss';
 
 import Appbar       from '../components/Appbar';
+import Nav          from '../components/Nav';
 import ContactBlock from '../components/ContactBlock';
 import Footer       from '../components/Footer';
 
@@ -12,10 +13,18 @@ class App extends Component {
       <div className="App">
         <Appbar />
 
-        {/* Page */}
-        {this.props.children}
+        <div className="container">
+          <div className="row">
+            <div className="col-2">
+              <Nav />
+            </div>{/* 30 */}
+            <div className="col-10">
+              {this.props.children}
 
-        <ContactBlock />
+              <ContactBlock />
+            </div>{/* 70 */}
+           </div>
+        </div>
 
         <Footer />
       </div>
