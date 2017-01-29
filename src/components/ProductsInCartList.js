931b3beb35a3bfbb0d460be8ac469f3707b3bdb9
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-29 16:45:27 -0300

    Update Products in cart component

--- src/components/ProductsInCartList.js
+++ src/components/ProductsInCartList.js
@@ -7,44 +7,53 @@ class ProductsInCartList extends Component {
         const data = this.props.data;
 
         return (
-            <ul className="ProductsInCartList">
-                {
-                    data.map((item) => {
-                        return (
-                            <li className="CartListItem" key={item.id}>
-                                <div className="description-container">
-                                    <picture>
-                                        <source srcSet={item.imageDesktop} media="(min-width: 40em)" />
-                                        <img src={item.image} alt={item.name} />
-                                    </picture>
+            <div className="ProductsInCart">
+                <ul className="ProductsInCartListHeader">
+                    <li>Producto</li>
+                    <li>Precio</li>
+                    <li>Cantidad</li>
+                    <li>Subtotal</li>
+                </ul>
 
-                                    <div className="product-info">
-                                        <h3>{item.name}</h3>
-                                        <p>{item.description}</p>
+                 <ul className="ProductsInCartList">
+                    {
+                        data.map((item) => {
+                            return (
+                                <li className="CartListItem" key={item.id}>
+                                    <div className="description-container">
+                                        <picture>
+                                            <source srcSet={item.imageDesktop} media="(min-width: 40em)" />
+                                            <img src={item.image} alt={item.name} />
+                                        </picture>
+
+                                        <div className="product-info">
+                                            <h3>{item.name}</h3>
+                                            <p>{item.description}</p>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <ul>
-                                    <li>
-                                        <strong>Precio</strong>
-                                        <span>${item.price}</span>
-                                    </li>
+                                    <ul>
+                                        <li>
+                                            <strong>Precio</strong>
+                                            <span>${item.price}</span>
+                                        </li>
 
-                                    <li>
-                                        <strong>Cantidad</strong>
-                                        <span>{item.quantity}</span>
-                                    </li>
+                                        <li>
+                                            <strong>Cantidad</strong>
+                                            <span>{item.quantity}</span>
+                                        </li>
 
-                                    <li>
-                                        <strong>SubTotal</strong>
-                                        <span>${item.price * item.quantity}</span>
-                                    </li>
-                                </ul>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+                                        <li>
+                                            <strong>SubTotal</strong>
+                                            <span>${item.price * item.quantity}</span>
+                                        </li>
+                                    </ul>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         );
     }
 
